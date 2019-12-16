3ae8f86b08f21cc6f14136444501202898d018ed
Theodrosrun <theodros.mulugeta@creatis.ch>
2019-12-16 14:24:05 +0100

    CREATIS Informatique

--- app/src/main/java/ch/creatis/bexio/LoginActivity.kt
+++ app/src/main/java/ch/creatis/bexio/LoginActivity.kt
@@ -2,9 +2,10 @@ package ch.creatis.bexio
 
 
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import com.android.volley.Request
@@ -12,6 +13,7 @@ import com.android.volley.Response
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import kotlinx.android.synthetic.main.activity_login.*
+import org.json.JSONObject
 
 
 
@@ -25,34 +27,9 @@ class LoginActivity : AppCompatActivity() {
 
 
 
-            webView.settings.javaScriptEnabled = true // enable javascript
-            webView.webViewClient = object : WebViewClient() {
-
-
-
-                override fun onPageFinished(view: WebView?, url: String?) {
-                    super.onPageFinished(view, url)
-
-
-
-                    if (url!!.contains("code")) {
-                        val chars = url
-                        val codeun = chars!!.dropLast(18)
-                        CODETOKEN = codeun.drop(16)
-
-
-
-                        webView.visibility = View.INVISIBLE
-                        getAccessToken()
-
-
-                    }
-                }
-            }
-
-
-
-            webView.loadUrl("https://office.bexio.com/oauth/authorize?client_id=4707003559.apps.bexio.com&redirect_uri=bxpocket://auth&state=creatis2019&scope=task_show%20project_show%20monitoring_edit")
+        val sharedPreferences = getSharedPreferences("Bexio", Context.MODE_PRIVATE)
+        var refreshToken = sharedPreferences.getString("REFRESHTOKEN", "")
+        if (refreshToken == ""){ webViewIsVisible() } else {getAccessTokenAllTime()}
 
 
 
@@ -60,17 +37,87 @@ class LoginActivity : AppCompatActivity() {
 
 
 
-    fun getAccessToken(){
+    fun webViewIsVisible(){
+
+        val sharedPreferences = getSharedPreferences("Bexio", Context.MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+
+
+
+        webView.settings.javaScriptEnabled = true // enable javascript
+        webView.webViewClient = object : WebViewClient() {
+
+
+
+            override fun onPageFinished(view: WebView?, url: String?) {
+                super.onPageFinished(view, url)
+
+
+
+                if (url!!.contains("code")) {
+
+
+                    val chars = url
+                    val codeun = chars!!.dropLast(18)
+
+
+
+                    // -------------------------------------------------------------------------------------
+
+                    editor.putString("CODETOKEN", codeun.drop(16))
+                    editor.commit()
+                    getAccessTokenFirstTime()
+//                    webView.visibility = View.INVISIBLE
+
+                    // -------------------------------------------------------------------------------------
+
+
+
+                }
+            }
+        }
+
+
+
+        webView.loadUrl("https://office.bexio.com/oauth/authorize?client_id=4707003559.apps.bexio.com&redirect_uri=bxpocket://auth&state=creatis2019&scope=task_show%20project_show%20monitoring_edit")
+
+
+
+    }
+
+
+
+    fun getAccessTokenFirstTime(){
+
+        val sharedPreferences = getSharedPreferences("Bexio", Context.MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+        var codeToken = sharedPreferences.getString("CODETOKEN", "")
 
 
 
         val queue = Volley.newRequestQueue(this)
 
-        val url = "https://office.bexio.com/oauth/access_token?client_id=4707003559.apps.bexio.com&redirect_uri=bxpocket://auth&client_secret=nRA79oKLwIIRh7NOH5TNavWabEE=&$CODETOKEN"
+        val url = "https://office.bexio.com/oauth/access_token?client_id=4707003559.apps.bexio.com&redirect_uri=bxpocket://auth&client_secret=nRA79oKLwIIRh7NOH5TNavWabEE=&$codeToken"
 
         val stringRequest = StringRequest(Request.Method.POST, url, Response.Listener<String> { response ->
 
-            println(response)
+            var responseJsonObj = JSONObject(response)
+
+
+            // -------------------------------------------------------------------------------------
+
+            editor.putString("ACCESSTOKEN", responseJsonObj.getString("access_token"))
+            editor.putString("REFRESHTOKEN", responseJsonObj.getString("refresh_token"))
+            editor.commit()
+
+
+
+            val intentAct = Intent(this@LoginActivity, MainActivity::class.java)
+            startActivity(intentAct)
+
+            // -------------------------------------------------------------------------------------
+
+
 
         }, Response.ErrorListener {})
 
@@ -78,20 +125,47 @@ class LoginActivity : AppCompatActivity() {
 
 
 
-//        val intentAct = Intent(this@LoginActivity, MainActivity::class.java)
-//        startActivity(intentAct)
-
-
-
     }
 
 
 
-    companion object {
+    fun getAccessTokenAllTime(){
 
-        var CODETOKEN = ""
-        var ACCESSTOKEN = ""
-        var REFRESHTOKEN = ""
+        val sharedPreferences = getSharedPreferences("Bexio", Context.MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+        var refreshToken = sharedPreferences.getString("REFRESHTOKEN", "")
+
+
+
+        val queue = Volley.newRequestQueue(this)
+
+        val url = "https://office.bexio.com/oauth/refresh_token?client_id=4707003559.apps.bexio.com&client_secret=nRA79oKLwIIRh7NOH5TNavWabEE=&refresh_token=$refreshToken"
+
+        val stringRequest = StringRequest(Request.Method.POST, url, Response.Listener<String> { response ->
+
+
+
+            // -------------------------------------------------------------------------------------
+
+            var responseJsonObj = JSONObject(response)
+            editor.putString("ACCESSTOKEN", responseJsonObj.getString("access_token"))
+            editor.putString("REFRESHTOKEN", responseJsonObj.getString("refresh_token"))
+            editor.commit()
+
+
+
+            val intentAct = Intent(this@LoginActivity, MainActivity::class.java)
+            startActivity(intentAct)
+
+            // -------------------------------------------------------------------------------------
+
+
+
+        }, Response.ErrorListener {})
+
+        queue.add(stringRequest)
+
+
 
     }
 
