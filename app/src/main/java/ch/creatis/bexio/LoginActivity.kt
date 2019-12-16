c289f70c759f21df413de8fcb322aa04012a5331
Theodrosrun <theodros.mulugeta@creatis.ch>
2019-12-16 11:53:17 +0100

    CREATIS Informatique

--- app/src/main/java/ch/creatis/bexio/LoginActivity.kt
+++ app/src/main/java/ch/creatis/bexio/LoginActivity.kt
@@ -1,11 +1,19 @@
 package ch.creatis.bexio
 
-import android.content.Intent
+
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import kotlinx.android.synthetic.main.activity_login.*
+
+
 
 class LoginActivity : AppCompatActivity() {
 
@@ -17,50 +25,76 @@ class LoginActivity : AppCompatActivity() {
 
 
 
-        loginButton.setOnClickListener {
-
             webView.settings.javaScriptEnabled = true // enable javascript
-
             webView.webViewClient = object : WebViewClient() {
+
 
 
                 override fun onPageFinished(view: WebView?, url: String?) {
                     super.onPageFinished(view, url)
 
 
-                    if (url!!.contains("code")) {
 
+                    if (url!!.contains("code")) {
                         val chars = url
                         val codeun = chars!!.dropLast(18)
+                        CODETOKEN = codeun.drop(16)
 
-                        CODEFINAL = codeun.drop(16)
 
+
+                        webView.visibility = View.INVISIBLE
+                        getAccessToken()
 
 
                     }
-
-
-
                 }
-
-
-
             }
+
 
 
             webView.loadUrl("https://office.bexio.com/oauth/authorize?client_id=4707003559.apps.bexio.com&redirect_uri=bxpocket://auth&state=creatis2019&scope=task_show%20project_show%20monitoring_edit")
 
 
-        }
+
+    }
+
+
+
+    fun getAccessToken(){
+
+
+
+        val queue = Volley.newRequestQueue(this)
+
+        val url = "https://office.bexio.com/oauth/access_token?client_id=4707003559.apps.bexio.com&redirect_uri=bxpocket://auth&client_secret=nRA79oKLwIIRh7NOH5TNavWabEE=&$CODETOKEN"
+
+        val stringRequest = StringRequest(Request.Method.POST, url, Response.Listener<String> { response ->
+
+            println(response)
+
+        }, Response.ErrorListener {})
+
+        queue.add(stringRequest)
+
+
+
+//        val intentAct = Intent(this@LoginActivity, MainActivity::class.java)
+//        startActivity(intentAct)
 
 
 
     }
+
+
 
     companion object {
 
-        var CODEFINAL = ""
+        var CODETOKEN = ""
+        var ACCESSTOKEN = ""
+        var REFRESHTOKEN = ""
 
     }
+
+
 
 }
